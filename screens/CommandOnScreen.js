0102ddf950df59fc6b27a578c36223c070393b72
Simon Kindström <simon.kindstrom@gmail.com>
2017-05-06 22:16:19 +0900

    Added duration parameters when creating on command

--- screens/CommandOnScreen.js
+++ screens/CommandOnScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
 
 import t from 'tcomb-form-native';
 
@@ -17,11 +17,15 @@ export default class CommandOnScreen extends Component {
 
     const { sender } = props.navigation.state.params;
 
-    let structObject = {};
+    let structObject = {
+      pin: t.String,
+      hours: t.Number,
+      minutes: t.Number,
+      seconds: t.Number
+    };
     for (let i = 1; i <= sender.outCount; ++i) {
       structObject['output ' + i] = t.Boolean;
     }
-    structObject['pin'] = t.String;
 
     this.formModel = t.struct(structObject);
     this.formOptions = {template: CommandOnTemplate, fields: {pin: {keyboardType: 'numeric', secureTextEntry: true}}};
@@ -53,6 +57,18 @@ function CommandOnTemplate(locals) {
       <View style={styles.checkboxContainer}>
         {getOutputs(locals.inputs)}
        </View>
+       <Text style={locals.stylesheet.controlLabel.normal}>Duration</Text>
+       <View style={styles.durationContainer}>
+         <View style={{flex: 1, marginRight: 5}}>
+         {locals.inputs.hours}
+         </View>
+         <View style={{flex: 1, marginRight: 5}}>
+         {locals.inputs.minutes}
+         </View>
+         <View style={{flex: 1}}>
+         {locals.inputs.seconds}
+         </View>
+      </View>
        <View>
         {locals.inputs.pin}
        </View>
@@ -83,5 +99,12 @@ const styles = StyleSheet.create({
   },
   checkbox: {
     width: '50%'
+  },
+  durationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  durationElement: {
+    flex: 1
   }
 });
