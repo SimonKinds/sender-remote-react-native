aa79da6e3e3f8430526a936bc503781c02a2e126
Simon Kindström <simon.kindstrom@gmail.com>
2017-05-11 18:03:01 +0900

    Now parses alarm status for ports

--- __tests__/PortStatusParser.test.js
+++ __tests__/PortStatusParser.test.js
@@ -1,41 +1,48 @@
 import { getValues } from '../common/PortStatusParser';
 
 test('Parse single status input', () => {
-  expect(getValues('STATUS: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%' });
+  expect(getValues('STATUS: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%', alarm: false  });
 });
 
 test('Parse single status output', () => {
-  expect(getValues('STATUS: OUT03=0')[0]).toEqual({ type: 'out', portNumber: 3, value: '0' });
+  expect(getValues('STATUS: OUT03=0')[0]).toEqual({ type: 'out', portNumber: 3, value: '0', alarm: false  });
 });
 
 test('Parse multiple ports', () => {
   expect(getValues('STATUS: IN01=+5C, IN05=-10, OUT01=0, IN99=10%,OUT11=3'))
     .toEqual([
-      { type: 'in', portNumber: 1, value: '+5C' },
-      { type: 'in', portNumber: 5, value: '-10' },
-      { type: 'in', portNumber: 99, value: '10%' },
-      { type: 'out', portNumber: 1, value: '0' },
-      { type: 'out', portNumber: 11, value: '3' }]);
+      { type: 'in', portNumber: 1, value: '+5C', alarm: false  },
+      { type: 'in', portNumber: 5, value: '-10', alarm: false  },
+      { type: 'in', portNumber: 99, value: '10%', alarm: false  },
+      { type: 'out', portNumber: 1, value: '0', alarm: false  },
+      { type: 'out', portNumber: 11, value: '3', alarm: false  }]);
 });
 
 test('Ports are sorted by type and then port numbers', () => {
   expect(getValues('STATUS: IN01=3, IN05=3, OUT99=1, IN02=1, OUT01=0'))
     .toEqual([
-      { type: 'in', portNumber: 1, value: '3' },
-      { type: 'in', portNumber: 2, value: '1' },
-      { type: 'in', portNumber: 5, value: '3' },
-      { type: 'out', portNumber: 1, value: '0' },
-      { type: 'out', portNumber: 99, value: '1' }]);
+      { type: 'in', portNumber: 1, value: '3', alarm: false  },
+      { type: 'in', portNumber: 2, value: '1', alarm: false  },
+      { type: 'in', portNumber: 5, value: '3', alarm: false  },
+      { type: 'out', portNumber: 1, value: '0', alarm: false  },
+      { type: 'out', portNumber: 99, value: '1', alarm: false  }]);
 });
 
 test('Can parse temperature', () => {
-  expect(getValues('Temperatur: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%' });
+  expect(getValues('Temperatur: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%', alarm: false});
 });
 
 test('Can parse humidity', () => {
-  expect(getValues('Humidity: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%' });
+  expect(getValues('Humidity: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%', alarm: false });
 });
 
 test('Can parse measurements', () => {
-  expect(getValues('Measurements: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%' });
+  expect(getValues('Measurements: IN01=3%')[0]).toEqual({ type: 'in', portNumber: 1, value: '3%', alarm: false });
+});
+
+test('Detects alarm', () => {
+  expect(getValues('Status: IN01=!3%, IN02=3'))
+    .toEqual([
+      { type: 'in', portNumber: 1, value: '3%', alarm: true },
+      { type: 'in', portNumber: 2, value: '3', alarm: false}]);
 });
--- common/PortStatusParser.js
+++ common/PortStatusParser.js
@@ -5,11 +5,12 @@ export function getValues(response) {
 
   const types = _.map(splits, val => val.toLowerCase().includes('in') ? 'in' : 'out');
   const portNumbers = _.map(splits, val => getPortNumber(val));
-  const values = _.map(splits, val => val.replace(/^[^=]+=/g, ''));
+  const values = _.map(splits, val => val.replace(/^[^=]+=!?/g, ''));
+  const alarms = _.map(splits, val => isAlarm(val));
 
-  const objectsAsLists = _.zip(types, portNumbers, values);
+  const objectsAsLists = _.zip(types, portNumbers, values, alarms);
 
-  const objects = _.map(objectsAsLists, list => ({ type: list[0], portNumber: list[1], value: list[2] }));
+  const objects = _.map(objectsAsLists, list => ({ type: list[0], portNumber: list[1], value: list[2], alarm: list[3] }));
 
   // in before out, it's lexicographical order
   return _.sortBy(objects, ['type', 'portNumber']);
@@ -19,4 +20,10 @@ function getPortNumber(val) {
   const regex = /\D*(?:IN|OUT)(\d+)=.+/g;
   const matches = regex.exec(val);
   return parseInt(matches[1]);
+}
+
+function isAlarm(val) {
+  const regex = /\D*(?:IN|OUT)\d+=(!)?.+/g;
+  const matches = regex.exec(val);
+  return matches[1] ? true : false;
 }
