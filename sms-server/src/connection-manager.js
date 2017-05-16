a23dc1150ee86f1404366f56c7d132065c3b48a6
Simon Kindström <simon.kindstrom@gmail.com>
2017-05-16 21:43:12 +0900

    Created a handler for websockets for numbers

---
+++ sms-server/src/connection-manager.js
@@ -0,0 +1,17 @@
+// @flow
+
+class ConnectionManager {
+  connections: {[number: string]: ?WebSocket}
+
+  pushConnection(number: string, ws: WebSocket) {
+    this.connections[number] = ws;
+  }
+
+  popConnection(number: string) {
+    this.connections[number] = null;
+  }
+
+  get(number: string) {
+    return this.connections[number];
+  }
+}
