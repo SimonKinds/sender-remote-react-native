30a727921e1dabe3ae2f9f005317728c8657571e
Simon Kindström <simon.kindstrom@gmail.com>
2017-05-18 10:05:05 +0900

    Added websocket connection to real SMS server

--- sms-server/src/connection-manager.js
+++ sms-server/src/connection-manager.js
@@ -1,7 +1,11 @@
 // @flow
 
 export default class ConnectionManager {
-  connections: {[number: string]: ?WebSocket}
+  connections: {[number: string]: ?WebSocket};
+
+  constructor() {
+    this.connections = {};
+  }
 
   pushConnection(number: string, ws: WebSocket) {
     this.connections[number] = ws;
--- sms-server/src/index.js
+++ sms-server/src/index.js
@@ -13,6 +13,44 @@ if (!twilioAuthToken) {
   process.exit(1);
 }
 
+const connectionManager = new ConnectionManager();
+
+const websocketPort = 9137;
+const websocketServer = new WebSocket.Server({ port: websocketPort });
+
+websocketServer.on('connection', (ws) => {
+  let number: ?string;
+
+  ws.on('open', () => {
+    console.log('Connection opened');
+  });
+
+  ws.on('message', (message) => {
+    const request: { to: ?string, msg: ?string } = JSON.parse(message);
+    if (request.to == null || request.msg == null) {
+      console.log('missing parameters');
+      return ws.close(4000, 'Missing parameters');
+    }
+
+    const { to, msg } = request;
+
+    connectionManager.pushConnection(to, ws);
+    number = to;
+
+    sendSms(to, msg, ws);
+  });
+
+  ws.on('close', () => {
+    if (number) {
+      connectionManager.popConnection(number);
+    }
+  });
+});
+
+function sendSms(to: string, message: string, ws: any) {
+  console.warn('Should send message with Twilio');
+}
+
 const httpPort = 9138;
 const app = express();
 // twilio response body is urlencoded
